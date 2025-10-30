d032a79129991c098714c22c68090d9f3dcdd274
Leandro Mateus <leandromateusmendes02@gmail.com>
2025-10-29 21:13:15 -0300

    pegando a classes de dominio feitas pelo artur

---
+++ bbh-project/models/src/main/java/bbh/domain/util/UsuarioTipo.java
@@ -0,0 +1,26 @@
+package bbh.domain.util;
+
+public enum UsuarioTipo {
+   ADMINISTRADOR,
+   TURISTA,
+   GUIA,
+   ESTABELECIMENTO;
+
+
+   public static UsuarioTipo strTo(String strTipo) {
+      switch (strTipo) {
+         case "ADMINISTRADOR" :
+            return ADMINISTRADOR;
+         case "TURISTA" :
+            return TURISTA;
+         case "GUIA" :
+            return GUIA;
+         case "ESTABELECIMENTO" :
+            return ESTABELECIMENTO;
+      }
+
+      throw new RuntimeException("O tipo:" + strTipo + " nao e valido");
+   }
+
+
+}
