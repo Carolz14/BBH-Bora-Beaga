79695efaf3ef13ca9a77eab06a04e9a60a590936
Leandro Mateus <leandromateusmendes02@gmail.com>
2025-11-03 22:18:15 -0300

    alterando novamente os xml do site e criando um db temporario para iniciar a fase de teste do meu csu #6

--- bbh-project/models/src/main/java/bbh/service/util/CriarTabelas.java
+++ bbh-project/models/src/main/java/bbh/service/util/CriarTabelas.java
@@ -5,6 +5,7 @@ import java.sql.Statement;
 import java.sql.SQLException;
 
 public class CriarTabelas {
+
     public static void criarTabelaTag() throws SQLException {
         String sql = "CREATE TABLE IF NOT EXISTS tag("
                 + "id BIGINT AUTO_INCREMENT PRIMARY KEY,"
@@ -17,23 +18,38 @@ public class CriarTabelas {
         statement.executeUpdate(sql);
         System.out.printf("Tabela tag criada (ou já existia previamente).\n");
     }
+
+    public static void criarTabelaUsuario() throws SQLException { /// apenas pro teste do meu csu
+        String sql = "CREATE TABLE IF NOT EXISTS usuario("
+                + "id BIGINT AUTO_INCREMENT PRIMARY KEY,"
+                + "nome VARCHAR(100) NOT NULL,"
+                + "email VARCHAR(100) UNIQUE NOT NULL"
+                + ");";
+        Connection con = ConexaoBD.getConnection();
+        Statement statement = con.createStatement();
+        statement.executeUpdate(sql);
+        System.out.printf("Tabela usuario criada (ou já existia previamente).\n");
+    }
+
     public static void criarTabelaCorrespondencia() throws SQLException {
         String sql = "CREATE TABLE IF NOT EXISTS tag_correspondencia("
-            + "id_usuario BIGINT,"
-            + "id_tag BIGINT,"
-            + "PRIMARY KEY (id_usuario, id_tag),"
-            + "FOREIGN KEY (id_usuario) REFERENCES usuario(id) "
-            + "ON DELETE CASCADE ON UPDATE CASCADE,"
-            + "FOREIGN KEY (id_tag) REFERENCES tag(id) "
-            + "ON DELETE CASCADE ON UPDATE CASCADE"
-            +");";
+                + "id_usuario BIGINT,"
+                + "id_tag BIGINT,"
+                + "PRIMARY KEY (id_usuario, id_tag),"
+                + "FOREIGN KEY (id_usuario) REFERENCES usuario(id) "
+                + "ON DELETE CASCADE ON UPDATE CASCADE,"
+                + "FOREIGN KEY (id_tag) REFERENCES tag(id) "
+                + "ON DELETE CASCADE ON UPDATE CASCADE"
+                + ");";
         Connection con = ConexaoBD.getConnection();
         Statement statement = con.createStatement();
         statement.executeUpdate(sql);
         System.out.printf("Tabela correspondência já criada (ou já existia previamente).\n");
     }
-    public static void criarTodasAsTabelas() throws SQLException{
+
+    public static void criarTodasAsTabelas() throws SQLException {
         criarTabelaTag();
+        criarTabelaUsuario();
         criarTabelaCorrespondencia();
     }
 }
